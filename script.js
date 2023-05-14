65696767f5c3162ac57b49595777b3e82047d4f7
lutpio <ahmadlutfi606@gmail.com>
2023-05-15 06:09:23 +0700

    push ke tujuh

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#lokasi").mouseup(function () {
+    $(location).attr("href", "https://www.google.com/");
+  });
+});
+
+$(document).ready(function () {
+  $("#wisata").mouseup(function () {
+    $(location).attr("href", "https://www.google.com/");
+  });
+});
