1ec1e6e221a5aca172628bdf98ac5a7a5990a93b
lutpio <ahmadlutfi606@gmail.com>
2023-05-15 22:59:54 +0800

    push ke sepuluh

--- script.js
+++ script.js
@@ -9,3 +9,47 @@ $(document).ready(function () {
     $(location).attr("href", "tour.html");
   });
 });
+
+function pesan() {
+  Swal.fire({
+    icon: "success",
+    title: "Pesan Telah Terkirim",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+
+  document.getElementById("from_name").value = "";
+  document.getElementById("message").value = "";
+  document.getElementById("email_id").value = "";
+}
+
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  
+  // these IDs from the previous steps
+  emailjs.sendForm("service_tx07oae", "template_5zftvfk", this).then(
+    function () {
+      pesan();
+      console.log("SUCCESS!");
+    },
+    function (error) {
+      console.log("FAILED...", error);
+    }
+  );
+});
+// function SendMail() {
+//   let params = {
+//     from_name: document.getElementById("nama").value,
+//     email_id: document.getElementById("email").value,
+//     message: document.getElementById("pesan").value,
+//   };
+//   emailjs.send("service_tx07oae", "template_5zftvfk", params).then(
+//     function (response) {
+//       pesan();
+//       console.log("SUCCESS!", response.status);
+//     },
+//     function (error) {
+//       console.log("FAILED...", error);
+//     }
+//   );
+// }
